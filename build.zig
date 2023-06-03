a41fd0277403b9bf6691423451db7a7eaf2405a5
Kristian Larsson <kristian@spritelink.net>
2023-06-03 15:41:57 +0200

    Remove ArrayList

--- build.zig
+++ build.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 const print = @import("std").debug.print;
-const ArrayList = std.ArrayList;
-
 
 pub fn build(b: *std.build.Builder) void {
     const optimize = b.standardOptimizeOption(.{});
